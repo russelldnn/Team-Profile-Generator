1f34e7a4ce94b1938e044090d6b98eb8a06f3cd6
russelldnn <russelldnn@gmail.com>
2022-07-19 06:52:25 -0500

    manager array questions

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
+//requirements needed for index
 let generateHTML = 0;
 let inquirer = require("inquirer");
 let fs = require("fs");
+let Employee = require("./lib/Employee");
+let Manager = require("./lib/Manager");
+let Intern = require("./lib/Intern");
+let Engineer = require("./lib/Engineer");
+
+//An array of questions for the manager in a format inquirer can use
+let managerInput = [
+    {
+        type:"input",
+        name:"name",
+        message:"Enter the managers name."
+    },
+    {
+        type:"input",
+        name:"email",
+        message:"enter the managers email."
+    },
+    {
+        type:"input",
+        name:"id",
+        message:"enter the managers ID with no spaces"
+    },
+    {
+        type:"input",
+        name:"officeNumber",
+        message:"enter the managers office number in the format XXX-XXX-XXXX"
+    },
+    
+]
